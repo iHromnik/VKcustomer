ca3fe20937c03bb852e4f3ee18137aeef200a24c
iHromnik <hromnik@me.com>
2022-04-13 20:58:13 +0300

    add logics to scroll

--- VKcustomer/LoginViewController.swift
+++ VKcustomer/LoginViewController.swift
@@ -2,9 +2,38 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var loginTextField: UITextField!
+        
+    @IBOutlet weak var passwordTextField: UITextField!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(keybordWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keybordWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        
+        let tabGesture = UITapGestureRecognizer(target: self, action: #selector(didTapOnScroll))
+        view.addGestureRecognizer(tabGesture)
+        view.isUserInteractionEnabled = true
+        
     }
     
+    @objc func keybordWillShow (notification: Notification) {
+        guard let kbSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.size.height, right: 0)
+        scrollView.contentInset = insets
+    }
+
+    @objc func keybordWillHide (notification: Notification) {
+        let insets = UIEdgeInsets.zero
+        scrollView.contentInset = insets
+    }
+    
+    @objc func didTapOnScroll() {
+        view.endEditing(true)
+    }
 }
